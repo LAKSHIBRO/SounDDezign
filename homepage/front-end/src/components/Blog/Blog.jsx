4e5b21af6d5b12f12b5a1138a8d69fcb66e5fc34
Lakshitha <notlakshitha@gmail.com>
2024-02-05 20:31:14 +0530

    Add blog and testimonials sections

--- homepage/front-end/src/components/Blog/Blog.jsx
+++ homepage/front-end/src/components/Blog/Blog.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
 import './Blog.css'
+import { blog1, blog2, blog3} from '../../assets'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <section className='pink' id='blog'>
+      <div className="wrapper">
+        <h2 className='light'>
+          Latest Posts
+        </h2>
+        <div className="content-container">
+          <div className="post">
+            <div className="tag">Daw</div>
+            <a href="#">
+              <img src={blog1} alt="" />
+            </a>
+            <a href="#">
+              <h3 className='post-title'>How To Use Drum Machine in Logic Pro X</h3>
+            </a>
+          </div>
+
+          <div className="post">
+            <div className="tag">Mixing</div>
+            <a href="#">
+              <img src={blog2} alt="" />
+            </a>
+            <a href="#">
+              <h3 className='post-title'>How To Mix Guitars Effectively
+</h3>
+            </a>
+          </div>
+
+          
+          <div className="post">
+            <div className="tag">VOX</div>
+            <a href="#">
+              <img src={blog3} alt="" />
+            </a>
+            <a href="#">
+              <h3 className='post-title'>The Real Power of Harmonies in Music Production</h3>
+            </a>
+          </div>
+          
+        </div>
+
+        <div className="btn-container">
+          <a href="#" className='all-posts-btn'>All Posts</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- homepage/front-end/src/components/Testimonials/Testimonials.jsx
+++ homepage/front-end/src/components/Testimonials/Testimonials.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import './Testimonials.css'
+import {Testimonial1, Testimonial2, Testimonial3} from '../../assets'
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <section className="dark-gray" id="testimonials">
+      <div className="wrapper">
+        <h2>What Our students say</h2>
+        <div className="content-container">
+
+          <div className="testimonial">
+            <img src={Testimonial1} alt="" />
+            <div className="reviewer-detais">
+              <div className="name">Peter Adams</div>
+              <div className="company-name">Google</div>
+              <div className="review">This is a great course. I got to learn a lot.</div>
+            </div>
+          </div>
+       
+
+          <div className="testimonial">
+            <img src={Testimonial2} alt="" />
+            <div className="reviewer-detais">
+              <div className="name">Robert Fox</div>
+              <div className="company-name">Meta</div>
+              <div className="review">I got to learn a lot about Music Production with this course. Thanks :)</div>
+            </div>
+          </div>
+        
+
+        
+          <div className="testimonial">
+            <img src={Testimonial3} alt="" />
+            <div className="reviewer-detais">
+              <div className="name">Emily Smith</div>
+              <div className="company-name">Apple</div>
+              <div className="review">Awesome! Great job!!</div>
+            </div>
+          
+        </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
