1467ffd1d916ce0c495debe8cafdaa117b2875b0
Lakshitha <notlakshitha@gmail.com>
2024-02-05 20:45:05 +0530

    Add footer with social media icons

--- homepage/front-end/src/components/Footer/Footer.jsx
+++ homepage/front-end/src/components/Footer/Footer.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import './Footer.css'
+import {facebookLogo, instagramLogo, twitterLogo} from '../../assets'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="black">
+      <div className="wrapper">
+        <div className="content-container">
+          <div className="links">
+            <a href="#" className='logo'>
+              Sound<span className='red'>DZign</span>
+            </a>
+
+            <div className="socail-icons">
+              <img src={facebookLogo} alt="" />
+            </div>
+            <div className="socail-icons">
+              <img src={instagramLogo} alt="" />
+            </div>
+            <div className="socail-icons">
+              <img src={twitterLogo} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
